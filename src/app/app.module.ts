1cd0dfe61ebbd065b93055dec06f1243ec893bbf
thork90 <vass90@gmail.com>
2021-09-14 18:09:55 +0200

    routing 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,21 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { JumbotronComponent } from './jumbotron/jumbotron.component';
 import { EventcardComponent } from './eventcard/eventcard.component';
 import { FooterComponent } from './footer/footer.component';
+import { EventComponent } from './event/event.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ProfileEditComponent } from './profile-edit/profile-edit.component';
+import { EventDetailComponent } from './event-detail/event-detail.component';
+import { EventListComponent } from './event-list/event-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { BidComponent } from './bid/bid.component';
+import { TicketDetailComponent } from './ticket-detail/ticket-detail.component';
+import { TicketListComponent } from './ticket-list/ticket-list.component';
+import { TicketComponent } from './ticket/ticket.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -14,11 +29,14 @@ import { FooterComponent } from './footer/footer.component';
     NavbarComponent,
     JumbotronComponent,
     EventcardComponent,
-    FooterComponent
+    FooterComponent,
+    
+    ...AppRoutingModule.routableComponents
   ],
   imports: [
     BrowserModule,
-    CollapseModule.forRoot()
+    CollapseModule.forRoot(),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
