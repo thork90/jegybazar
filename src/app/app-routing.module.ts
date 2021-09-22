98c5852f963572e8acd25592838fa2e5864387c1
thork90 <vass90@gmail.com>
2021-09-22 19:55:33 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,52 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
 import { EventComponent } from './event/event.component';
 import { TicketComponent } from './ticket/ticket.component';
 import { AboutComponent } from './about/about.component';
-import { LoginComponent } from './login/login.component';
-import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegistrationComponent } from './user/registration/registration.component';
+import { EventListComponent } from './event/event-list/event-list.component';
+import { EventDetailComponent } from './event/event-detail/event-detail.component';
+import { ProfileComponent } from './user/profile/profile.component';
+import { ProfileEditComponent } from './user/profile-edit/profile-edit.component';
+import { TicketListComponent } from './ticket/ticket-list/ticket-list.component';
+import { TicketDetailComponent } from './ticket/ticket-detail/ticket-detail.component';
 
 
 
 const routes: Routes = [
     { path: 'home', component: HomeComponent },
-    {path: 'event', component: EventComponent},
-    {path: 'ticket', component: TicketComponent},
-    {path: 'about', component: AboutComponent},
-    {path: 'login', component: LoginComponent},
-    {path: 'registration', component: RegistrationComponent},
-    { path: '', redirectTo:'/home', pathMatch: 'full'  },
-    {path: '**', component: PageNotFoundComponent}
+    {
+        path: 'event', component: EventComponent, children: [
+            { path: 'list', component: EventListComponent },
+            { path: 'new', component: EventDetailComponent }, 
+            { path: ':id', component: EventDetailComponent }
+        ]
+    },
+    {
+        path: 'ticket', component: TicketComponent, children: [
+            { path: 'list', component: TicketListComponent },
+            { path: 'new', component: TicketDetailComponent },
+            { path: ':id/bid', component: TicketDetailComponent }
+        ]
+
+    },
+    { path: 'about', component: AboutComponent },
+
+    {
+        path: 'user', children: [
+
+            { path: '', component: ProfileComponent },
+            { path: 'edit', component: ProfileEditComponent },
+            { path: 'login', component: LoginComponent },
+            { path: 'registration', component: RegistrationComponent }
+
+        ]
+    },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: '**', component: PageNotFoundComponent }
 
 ];
 
@@ -30,9 +58,16 @@ export class AppRoutingModule {
     static routableComponents = [
         HomeComponent,
         EventComponent,
+        TicketListComponent,
+        EventListComponent,
+        TicketDetailComponent,
+        EventDetailComponent,
+        TicketDetailComponent,
         TicketComponent,
         AboutComponent,
         LoginComponent,
+        ProfileComponent,
+        ProfileEditComponent,
         RegistrationComponent,
         PageNotFoundComponent
 
